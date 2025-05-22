8610d648c1851b1fff92990a5e3817e1e712e918
DaniloGC <chinendanilo@gmail.com>
2025-05-21 21:54:43 -0300

    started integration

---
+++ processador/compile_all.bat
@@ -0,0 +1,6 @@
+call compile_ULA.bat
+call compile_bank_regs.bat
+call compile_bank_ULA.bat
+call compile_rom.bat
+call compile_pc_uc_tb.bat
+call compile_state_machine.bat
--- processador/pc_uc.vhd
+++ processador/pc_uc.vhd
@@ -32,17 +32,26 @@ architecture a_pc_uc of pc_uc is
             dado     : out unsigned(16 downto 0)
       );
    end component;
+   component state_machine is
+      port( clk      : in std_logic;
+            rst      : in std_logic;
+            data_out : out std_logic
+      );
+   end component;
 
    signal out_pc: unsigned(6 downto 0);
    signal out_uc: unsigned(6 downto 0);
    signal out_rom: unsigned(16 downto 0);
+   signal out_sm: std_logic;
+   signal wr_en_pc: std_logic;
+   signal rom_clk: std_logic;
 begin
 
    pc_inst: pc
       port map(
          clk      => clk,
          rst      => rst,
-         wr_en    => '1',
+         wr_en    => wr_en_pc,
          data_in  => out_uc,
          data_out => out_pc
       );
@@ -58,9 +67,19 @@ begin
 
    rom_inst: rom
       port map(
-         clk      => clk,
+         clk      => rom_clk,
          endereco => out_pc,
          dado     => out_rom
       );
+
+   sm_inst: state_machine
+      port map(
+         clk      => clk,
+         rst      => rst,
+         data_out => out_sm
+      );
+   
+   wr_en_pc <= '1' when out_sm = '1' else '0';
+   rom_clk <= '0' when out_sm = '1' else '1';
 
 end architecture;
